9809d21f2be6be17e76256de627df42026ca6049
Róbert Klimant <roboklimant@gmail.com>
2024-09-25 18:34:32 +0200

    feat: onscroll nav animation

---
+++ src/utils/scrollDetection.ts
@@ -0,0 +1,45 @@
+export const scrollDetection = () => {
+  const navEl = document.querySelector('#navbar') as HTMLElement;
+  const navLogo = document.querySelector('#navLogo') as HTMLElement;
+  const navLink = document.querySelectorAll('#navLink') as NodeListOf<HTMLElement>;
+  const navButt = document.querySelector('[data-button="navButt"]') as HTMLElement;
+
+  const sections = document.querySelectorAll('[scroll-detection]') as NodeListOf<HTMLElement>;
+
+  let activeSection: HTMLElement | null = null; // Premenná na sledovanie aktívnej sekcie
+
+  const handleScroll = () => {
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+
+      // Ak vrch sekcie dosiahne vrch obrazovky (scrollujeme nadol) alebo vstúpime zo spodnej časti obrazovky (scrollujeme hore)
+      if (rect.top <= 50 && rect.bottom > 60 && activeSection !== section) {
+        // console.log('Sekcia narazila na vrch obrazovky alebo vstúpila z dola:', section);
+        navEl.style.backgroundColor = '#0B0B0D4D';
+        navLogo.style.color = '#FEFEFE';
+        navLink.forEach((el) => {
+          el.style.color = '#FEFEFE';
+        });
+        navButt.setAttribute('button-mode', 'green');
+        activeSection = section; // Nastavíme aktuálnu sekciu ako aktívnu
+      }
+
+      // Ak opustíš sekciu (buď vrchom alebo spodkom obrazovky)
+      if ((rect.top > 70 || rect.bottom <= 50) && activeSection === section) {
+        navEl.style.backgroundColor = '#FFFFFF4D';
+        navLogo.style.color = '#0B0B0D';
+        navLink.forEach((el) => {
+          el.style.color = '#0B0B0D';
+        });
+        navButt.removeAttribute('button-mode');
+        activeSection = null; // Zresetujeme aktívnu sekciu
+      }
+    });
+  };
+
+  // Pripojíme listener na udalosť scroll
+  window.addEventListener('scroll', handleScroll);
+
+  // Spustíme kontrolu hneď po načítaní stránky
+  window.addEventListener('load', handleScroll);
+};
