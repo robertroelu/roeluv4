765a90baff28444d889683f1f3ab0434621c4f66
Róbert Klimant <roboklimant@gmail.com>
2024-09-26 15:57:27 +0200

    fix: change to scroll

--- src/utils/fillAnimation.ts
+++ src/utils/fillAnimation.ts
@@ -1,3 +1,37 @@
+// import SplitType from 'split-type';
+// import { gsap } from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// export const fillAnimation = () => {
+//   gsap.registerPlugin(ScrollTrigger);
+
+//   const scrollEl = document.querySelectorAll('[scroll-animation]') as NodeListOf<HTMLElement>;
+//   if (!scrollEl) return;
+
+//   scrollEl.forEach((el) => {
+//     let typeSplit = new SplitType(el, {
+//       types: 'lines,words,chars',
+//       tagName: 'span',
+//     });
+
+//     let tl = gsap.timeline({ paused: true }); // Create a paused timeline
+
+//     tl.from(el.querySelectorAll('.word'), {
+//       opacity: 0.3,
+//       duration: 0.75,
+//       ease: 'power4.out',
+//       stagger: { each: 0.15 },
+//     });
+
+//     ScrollTrigger.create({
+//       trigger: el,
+//       start: 'top 80%', // When the top of the element hits 70% of the viewport
+//       onEnter: () => tl.play(), // Play the animation once on entering
+//       once: true, // Ensure it only triggers once
+//     });
+//   });
+// };
+
 import SplitType from 'split-type';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -7,27 +41,25 @@ export const fillAnimation = () => {
 
   const scrollEl = document.querySelectorAll('[scroll-animation]') as NodeListOf<HTMLElement>;
   if (!scrollEl) return;
-
   scrollEl.forEach((el) => {
     let typeSplit = new SplitType(el, {
       types: 'lines,words,chars',
       tagName: 'span',
     });
 
-    let tl = gsap.timeline({ paused: true }); // Create a paused timeline
-
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: el,
+        start: 'top 95%',
+        end: 'bottom 20%',
+        scrub: true,
+      },
+    });
     tl.from(el.querySelectorAll('.word'), {
       opacity: 0.3,
-      duration: 0.75,
+      duration: 2.5,
       ease: 'power4.out',
-      stagger: { each: 0.15 },
-    });
-
-    ScrollTrigger.create({
-      trigger: el,
-      start: 'top 80%', // When the top of the element hits 70% of the viewport
-      onEnter: () => tl.play(), // Play the animation once on entering
-      once: true, // Ensure it only triggers once
+      stagger: { each: 0.3 },
     });
   });
 };
