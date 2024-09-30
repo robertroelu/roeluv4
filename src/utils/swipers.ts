9dcb0e16a4392a59ca8867aef9004e2d13a96ba1
Róbert Klimant <roboklimant@gmail.com>
2024-09-30 12:32:14 +0200

    fix: change swiper from fade to swipe

--- src/utils/swipers.ts
+++ src/utils/swipers.ts
@@ -22,74 +22,78 @@ export const swipers = () => {
       const testimonialSwiper = new Swiper(el as HTMLElement, {
         modules: swiperModules,
         slidesPerView: 2,
-        slidesPerGroup: 2,
+        // slidesPerGroup: 2,
         // loop: true,
-        rewind: true,
-        speed: 0,
+        // rewind: true,
+        speed: 600,
         spaceBetween: 24,
         allowTouchMove: false,
         breakpoints: {
           320: {
             slidesPerView: 1,
-            slidesPerGroup: 1,
+            // slidesPerGroup: 1,
           },
           767: {
             slidesPerView: 2,
-            slidesPerGroup: 2,
+            // slidesPerGroup: 2,
           },
+        },
+        navigation: {
+          nextEl: swiperNext,
+          prevEl: swiperPrev,
         },
       });
 
-      // Opcity to 1 after window is fully load
-      window.onload = () => {
-        // Code to run when the page has fully loaded
-        const firstLoad = document.querySelectorAll(
-          '.swiper-slide-active, .swiper-slide-next'
-        ) as NodeListOf<HTMLElement>;
-        firstLoad.forEach((el) => {
-          el.style.opacity = '1';
-        });
-      };
+      // // Opcity to 1 after window is fully load
+      // window.onload = () => {
+      //   // Code to run when the page has fully loaded
+      //   const firstLoad = document.querySelectorAll(
+      //     '.swiper-slide-active, .swiper-slide-next'
+      //   ) as NodeListOf<HTMLElement>;
+      //   firstLoad.forEach((el) => {
+      //     el.style.opacity = '1';
+      //   });
+      // };
 
-      // Amimation for slide
-      function animation() {
-        if (swiperNext?.hasAttribute('data-disabled') && swiperPrev?.hasAttribute('data-disabled'))
-          return;
+      // // Amimation for slide
+      // function animation() {
+      //   if (swiperNext?.hasAttribute('data-disabled') && swiperPrev?.hasAttribute('data-disabled'))
+      //     return;
 
-        // Set custom attribute to prevent interaction
-        if (swiperNext) swiperNext.setAttribute('data-disabled', '');
-        if (swiperPrev) swiperPrev.setAttribute('data-disabled', '');
+      //   // Set custom attribute to prevent interaction
+      //   if (swiperNext) swiperNext.setAttribute('data-disabled', '');
+      //   if (swiperPrev) swiperPrev.setAttribute('data-disabled', '');
 
-        const activeSlides = document.querySelectorAll(
-          '.swiper-slide-active, .swiper-slide-next'
-        ) as NodeListOf<HTMLElement>;
+      //   const activeSlides = document.querySelectorAll(
+      //     '.swiper-slide-active, .swiper-slide-next'
+      //   ) as NodeListOf<HTMLElement>;
 
-        activeSlides.forEach((slide) => {
-          slide.style.opacity = '0'; // Fade out
-        });
-        setTimeout(() => {
-          testimonialSwiper.slideNext();
+      //   activeSlides.forEach((slide) => {
+      //     slide.style.opacity = '0'; // Fade out
+      //   });
+      //   setTimeout(() => {
+      //     testimonialSwiper.slideNext();
 
-          let activeSlidesAnimation = document.querySelectorAll(
-            '.swiper-slide-active, .swiper-slide-next'
-          ) as NodeListOf<HTMLElement>;
-          activeSlidesAnimation.forEach((slide) => {
-            slide.style.opacity = '1'; // Fade out
-          });
+      //     let activeSlidesAnimation = document.querySelectorAll(
+      //       '.swiper-slide-active, .swiper-slide-next'
+      //     ) as NodeListOf<HTMLElement>;
+      //     activeSlidesAnimation.forEach((slide) => {
+      //       slide.style.opacity = '1'; // Fade out
+      //     });
 
-          // Remove custom attribute to allow interaction
-          if (swiperNext) swiperNext.removeAttribute('data-disabled');
-          if (swiperPrev) swiperPrev.removeAttribute('data-disabled');
-        }, 400);
-      }
+      //     // Remove custom attribute to allow interaction
+      //     if (swiperNext) swiperNext.removeAttribute('data-disabled');
+      //     if (swiperPrev) swiperPrev.removeAttribute('data-disabled');
+      //   }, 400);
+      // }
 
-      swiperNext?.addEventListener('click', () => {
-        animation();
-      });
+      // swiperNext?.addEventListener('click', () => {
+      //   animation();
+      // });
 
-      swiperPrev?.addEventListener('click', () => {
-        animation();
-      });
+      // swiperPrev?.addEventListener('click', () => {
+      //   animation();
+      // });
     }
   });
 };
